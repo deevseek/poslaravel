a305574f49687c5130647c7b82d0df8aa860c46c
deevseek <nugraha.deev@gmail.com>
2025-12-12 13:37:58 +0700

    Remove email verification requirement

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,13 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
