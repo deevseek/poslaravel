73429840512c7192e595f76ff569376d616bdcc3
deevseek <nugraha.deev@gmail.com>
2025-12-29 18:16:15 +0700

    Allow localhost as central domain

--- config/tenancy.php
+++ config/tenancy.php
@@ -5,6 +5,8 @@ return [
     'tenant_connection' => 'tenant',
     'central_domains' => [
         env('APP_DOMAIN', 'profesionalservis.my.id'),
+        'localhost',
+        '127.0.0.1',
     ],
     'tenant_database_prefix' => env('TENANT_DB_PREFIX', 'tenant_'),
     'tenant_migrations_paths' => [
