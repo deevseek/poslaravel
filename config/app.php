19c407bdf8cb0c82be4572cb5fc10457f5aeb463
deevseek <nugraha.deev@gmail.com>
2026-01-01 19:12:12 +0700

    Update application name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Dealer Management'),
+    'name' => env('APP_NAME', 'Profesional Servis'),
 
     /*
     |--------------------------------------------------------------------------
